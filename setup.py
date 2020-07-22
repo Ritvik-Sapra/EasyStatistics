5820d267181919076d018efe5f986d45a1543394
Ritvik Sapra <sapra.ritvik@outlook.com>
2020-07-22 10:22:08 +0530

    Version 0.4 of the package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='EasyStatistics',
+      version='0.4',
+      description='Making Gaussian and Binomial distributions easy to work with.',
+      author_name = 'Ritvik Sapra',
+      author_email = 'sapra.ritvik@outlook.com',
+      packages=['EasyStatistics'],
+      zip_safe=False)
